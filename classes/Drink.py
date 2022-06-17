fceb7d27a01974692f7c7ede888aa72225049aea
akasz56 <indo14nurfath@apps.ipb.ac.id>
2022-06-17 22:18:13 +0700

    drink crud added

--- classes/Drink.py
+++ classes/Drink.py
@@ -1,6 +1,12 @@
 import enum
 from pydantic import BaseModel
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from database.db import Drink_DB, get_db
 from classes.Menu import Menu
+
+
+# ------------------------ Enum
 
 
 class DRINK_CATEGORY(enum.Enum):
@@ -20,16 +26,76 @@ class DRINK_SIZE(enum.Enum):
     large = 3
 
 
-class Drink(Menu):
-    category: DRINK_CATEGORY
-    type: DRINK_TYPE
-    size: DRINK_SIZE
+# ------------------------ Schema
 
 
 class DrinkCreate(BaseModel):
     name: str
     description: str
     price: int
+    category: int
+    type: int
+    size: int
+
+
+# ------------------------ Class
+
+
+class Drink(Menu):
     category: DRINK_CATEGORY
     type: DRINK_TYPE
     size: DRINK_SIZE
+
+    def get_all(db: Session = Depends(get_db)):
+        drinks = db.query(Drink_DB).all()
+        return drinks
+
+    def get_one(id: int, db: Session = Depends(get_db)):
+        drink = db.query(Drink_DB).filter(Drink_DB.id == id).first()
+        if not drink:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail=f"Drink with id {id} not found")
+
+        return drink
+
+    def create(request: DrinkCreate, db: Session = Depends(get_db)):
+        drink = Drink_DB(
+            name=request.name,
+            description=request.description,
+            price=request.price,
+            category=DRINK_CATEGORY(request.category).name,
+            type=DRINK_TYPE(request.type).name,
+            size=DRINK_SIZE(request.size).name
+        )
+        db.add(drink)
+        db.commit()
+        db.refresh(drink)
+
+        return drink
+
+    def update(id: int, request: DrinkCreate, db: Session = Depends(get_db)):
+        drink = db.query(Drink_DB).filter(Drink_DB.id == id).first()
+        if not drink:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail=f"Drink with id {id} not found")
+
+        drink.name = request.name,
+        drink.description = request.description,
+        drink.price = request.price,
+        drink.category = DRINK_CATEGORY(request.category).name,
+        drink.type = DRINK_TYPE(request.type).name,
+        drink.size = DRINK_SIZE(request.size).name
+        db.commit()
+        db.refresh(drink)
+
+        return drink
+
+    def delete(id: int,  db: Session = Depends(get_db)):
+        drink = db.query(Drink_DB).filter(Drink_DB.id == id)
+        exist = drink.first()
+        if not exist:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail=f"Buyer with id {id} not found")
+
+        drink.delete()
+        return id
--- route/drink_routes.py
+++ route/drink_routes.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, Response, status
+from sqlalchemy.orm import Session
+from classes.Drink import Drink, DrinkCreate
+from database.db import get_db
 
 
 router = APIRouter(
@@ -8,25 +11,33 @@ router = APIRouter(
 
 
 @router.get('/')
-async def drink_get_all():
-    return {"Return": "Success"}
+async def buyer_get_all(db: Session = Depends(get_db)):
+    return Drink.get_all(db)
 
 
 @router.get('/{id}')
-async def drink_get_one(id):
-    return {"Return": "Success " + id}
+async def buyer_get_one(id: int, db: Session = Depends(get_db)):
+    return Drink.get_one(id, db)
 
 
 @router.post('/')
-async def drink_create():
-    return {"Return": "Success"}
+async def buyer_create(request: DrinkCreate, response: Response, db: Session = Depends(get_db)):
+    try:
+        buyer = Drink.create(request, db)
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Something Happened during creating Drink")
+
+    response.status_code = status.HTTP_201_CREATED
+    return buyer
 
 
 @router.put('/{id}')
-async def drink_update(id):
-    return {"Return": "Success " + id}
+async def buyer_update(id: int, request: DrinkCreate, db: Session = Depends(get_db)):
+    return Drink.update(id, request, db)
 
 
 @router.delete('/{id}')
-async def drink_delete(id):
-    return {"Return": "Success " + id}
+async def buyer_delete(id: int,  db: Session = Depends(get_db)):
+    drinkid = Drink.delete(id, db)
+    return {"detail": f"Drink with id {drinkid} successfully deleted"}
