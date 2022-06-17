c78fd0baa79962e016fd315c853ae742b09353c8
akasz56 <indo14nurfath@apps.ipb.ac.id>
2022-06-18 03:48:10 +0700

    order CRUD added

--- controller/orderController.py
+++ controller/orderController.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Depends, HTTPException, Response, status
+from sqlalchemy.orm import Session
+from models.Order import Order, OrderCreate
+from database.db import get_db
 
 router = APIRouter(
     prefix="/order",
@@ -8,25 +10,33 @@ router = APIRouter(
 
 
 @router.get('/')
-async def order_get_all():
-    return {"Return": "Success"}
+async def order_get_all(db: Session = Depends(get_db)):
+    return Order.get_all(db)
 
 
 @router.get('/{id}')
-async def order_get_one(id):
-    return {"Return": "Success " + id}
+async def order_get_one(id: int, db: Session = Depends(get_db)):
+    return Order.get_one(id, db)
 
 
 @router.post('/')
-async def order_create():
-    return {"Return": "Success"}
+async def order_create(request: OrderCreate, response: Response, db: Session = Depends(get_db)):
+    try:
+        order = Order.create(request, db)
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Something Happened during creating Order")
+
+    response.status_code = status.HTTP_201_CREATED
+    return order
 
 
 @router.put('/{id}')
-async def order_update(id):
-    return {"Return": "Success " + id}
+async def order_update(id: int, request: OrderCreate, db: Session = Depends(get_db)):
+    return Order.update(id, request, db)
 
 
 @router.delete('/{id}')
-async def order_delete(id):
-    return {"Return": "Success " + id}
+async def order_delete(id: int,  db: Session = Depends(get_db)):
+    orderid = Order.delete(id, db)
+    return {"detail": f"Order with id {orderid} successfully deleted"}
--- models/Order.py
+++ models/Order.py
@@ -1,12 +1,98 @@
 import datetime
+import enum
+from typing import List
 from pydantic import BaseModel
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from database.db import Order_DB, get_db
 from models.Buyer import Buyer
 from models.Menu import Menu
 
 
+# ------------------------ Enum
+
+class ORDER_STATUS(enum.Enum):
+    pending = 1
+    waiting = 2
+    processing = 3
+    done = 4
+
+
+# ------------------------ Schema
+
+
+class OrderMenu():
+    menu_class: str
+    menu_id: int
+
+
+class OrderCreate(BaseModel):
+    status: int
+    total_price: int
+    drink: list[int]
+    food: list[int]
+    buyer_id: int
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
+# ------------------------ Class
+
+
 class Order():
     status: str
-    total_price: str
+    total_price: int
     datetime: datetime.datetime
-    menu: Menu
+    drink: list[int]
+    food: list[int]
     buyer: Buyer
+
+    def get_all(db: Session = Depends(get_db)):
+        orders = db.query(Order_DB).all()
+        return orders
+
+    # def get_one(id: int, db: Session = Depends(get_db)):
+        # order = db.query(Order_DB).filter(Order_DB.id == id).first()
+        # if not order:
+        #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        #                         detail=f"Order with id {id} not found")
+
+        # return order
+
+    def create(request: OrderCreate, db: Session = Depends(get_db)):
+        return request
+        # order = Order_DB(
+        #     status=ORDER_STATUS(request.status).name,
+        #     buyer_id=request.buyer_id
+        # )
+        # db.add(order)
+        # db.commit()
+        # db.refresh(order)
+
+        # return order
+
+    # def update(id: int, request: OrderCreate, db: Session = Depends(get_db)):
+        # order = db.query(Order_DB).filter(Order_DB.id == id).first()
+        # if not order:
+        #     raise HTTPException(
+        #         status_code=status.HTTP_404_NOT_FOUND, detail=f"Order with id {id} not found")
+
+        # order.status = ORDER_STATUS(request.status).name,
+        # order.buyer_id = request.buyer_id
+        # db.commit()
+        # db.refresh(order)
+
+        # return order
+
+    # def delete(id: int,  db: Session = Depends(get_db)):
+        # order = db.query(Order_DB).filter(Order_DB.id == id)
+        # exist = order.first()
+        # if not exist:
+        #     raise HTTPException(
+        #         status_code=status.HTTP_404_NOT_FOUND, detail=f"Buyer with id {id} not found")
+
+        # order.delete()
+        # db.commit()
+
+        # return id
