cf3cfffe383b1d39affdedc430e1fc12c04a3fc0
akasz56 <indo14nurfath@apps.ipb.ac.id>
2022-06-17 23:26:01 +0700

    food CRUD added

--- classes/Food.py
+++ classes/Food.py
@@ -1,6 +1,12 @@
 import enum
 from pydantic import BaseModel
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from database.db import Food_DB, get_db
 from classes.Menu import Menu
+
+
+# ------------------------ Enum
 
 
 class FOOD_CATEGORY(enum.Enum):
@@ -10,12 +16,71 @@ class FOOD_CATEGORY(enum.Enum):
     others = 4
 
 
-class Food(Menu):
-    category: FOOD_CATEGORY
+# ------------------------ Schema
 
 
 class FoodCreate(BaseModel):
     name: str
     description: str
     price: int
+    category: int
+
+
+# ------------------------ Class
+
+
+class Food(Menu):
     category: FOOD_CATEGORY
+
+    def get_all(db: Session = Depends(get_db)):
+        foods = db.query(Food_DB).all()
+        return foods
+
+    def get_one(id: int, db: Session = Depends(get_db)):
+        food = db.query(Food_DB).filter(Food_DB.id == id).first()
+        if not food:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail=f"Drink with id {id} not found")
+
+        return food
+
+    def create(request: FoodCreate, db: Session = Depends(get_db)):
+        food = Food_DB(
+            name=request.name,
+            description=request.description,
+            price=request.price,
+            category=FOOD_CATEGORY(request.category).name,
+        )
+        db.add(food)
+        db.commit()
+        db.refresh(food)
+
+        return food
+
+    def update(id: int, request: FoodCreate, db: Session = Depends(get_db)):
+        food = db.query(Food_DB).filter(Food_DB.id == id).first()
+        if not food:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail=f"Drink with id {id} not found")
+
+        food.name = request.name,
+        food.description = request.description,
+        food.price = request.price,
+        food.category = FOOD_CATEGORY(request.category).name,
+        print("1")
+        db.commit()
+        print("2")
+        db.refresh(food)
+        print("3")
+
+        return food
+
+    def delete(id: int,  db: Session = Depends(get_db)):
+        food = db.query(Food_DB).filter(Food_DB.id == id)
+        exist = food.first()
+        if not exist:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail=f"Buyer with id {id} not found")
+
+        food.delete()
+        return id
--- route/food_routes.py
+++ route/food_routes.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, Response, status
+from sqlalchemy.orm import Session
+from classes.Food import Food, FoodCreate
+from database.db import get_db
 
 
 router = APIRouter(
@@ -8,25 +11,33 @@ router = APIRouter(
 
 
 @router.get('/')
-async def food_get_all():
-    return {"Return": "Success"}
+async def buyer_get_all(db: Session = Depends(get_db)):
+    return Food.get_all(db)
 
 
 @router.get('/{id}')
-async def food_get_one(id):
-    return {"Return": "Success " + id}
+async def buyer_get_one(id: int, db: Session = Depends(get_db)):
+    return Food.get_one(id, db)
 
 
 @router.post('/')
-async def food_create():
-    return {"Return": "Success"}
+async def buyer_create(request: FoodCreate, response: Response, db: Session = Depends(get_db)):
+    try:
+        buyer = Food.create(request, db)
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Something Happened during creating Food")
+
+    response.status_code = status.HTTP_201_CREATED
+    return buyer
 
 
 @router.put('/{id}')
-async def food_update(id):
-    return {"Return": "Success " + id}
+async def buyer_update(id: int, request: FoodCreate, db: Session = Depends(get_db)):
+    return Food.update(id, request, db)
 
 
 @router.delete('/{id}')
-async def food_delete(id):
-    return {"Return": "Success " + id}
+async def buyer_delete(id: int,  db: Session = Depends(get_db)):
+    drinkid = Food.delete(id, db)
+    return {"detail": f"Food with id {drinkid} successfully deleted"}
